c980390ae108b05b2142bf6db133d1ad273899d3
Christopher Thielen <chris@epiar.net>
2011-06-08 14:03:59 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CartMachine::Application.config.secret_token = '44804d23c99f9f7cdb46157e01e72a8321cdc36f427bf876676bac1ca928e210a23f02d087a886538c73ddeb94ebb831b606bb35d7b4c23f94cc29f6add61a18'
